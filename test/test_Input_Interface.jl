981d42c9a8cb3b9315fcbca89a71705180aceea7
Mohamed <DrKrar@users.noreply.github.com>
2017-05-03 00:12:39 +0200

    Update test_Input_Interface.jl

--- test/test_Input_Interface.jl
+++ test/test_Input_Interface.jl
@@ -9,27 +9,32 @@
 
 # this set of tests need interactive input from the console, uncomment the line after `while` to do tests.
 info("Statrting `getfloat` test...")
-warn("those tests are suppressed because it need interactive input")
-#dotest=false
-@testset "Input_Interface" begin
-@testset "getfloat" begin
-	info("test `getfloat` with different ways to input numbers...")
-	@test_skip   getfloat("\njust press return: ") == 0.0 
-	@test_skip   getfloat("\ninput 1, then press return: ") == 1.0
-	@test_skip   getfloat("\ninput 1.0, then press return: ") == 1.0 
-	@test_skip   getfloat("\ninput '2e3', then press return: ") == 2000.0
-	@test_skip   getfloat("\ninput '3.4e-2', then press return: ") == 0.034
-	@test_skip   typeof(getfloat("\ntry to input any string, only valid number should accepted: ")) == Float64
-	@test_skip   typeof(getfloat("\nthe first time input '1.2f': ")) == Float64
-	
-	info("test `getfloat` with mathematical expressions...")
-	@test_skip   getfloat("\ninput 1/2, then press return: ") == 0.5
-	@test_skip   getfloat("\ninput 'pi/2', then press return: ") == pi/2
-	@test_skip   getfloat("\ninput 'e', then press return: ") == e
-	@test_skip   getfloat("\ninput 'e^3', then press return: ") == e^3
-	@test_skip   getfloat("\ninput 'sin(0.1)', then press return: ") == sin(0.1)
-	@test_skip   typeof(getfloat("\nthe first time input '1.2+2im': ")) == Float64
 
-end # testset
+warn("those tests are suppressed because it need interactive input")
+const dotest = false
+
+@testset "Input_Interface" begin
+    @testset "getfloat" begin
+	if dotest
+		info("test `getfloat` with different ways to input numbers...")
+		@test   0.0     ==  getfloat("\njust press return: ")
+		@test   1.0     ==  getfloat("\ninput 1, then press return: ")
+	    @test   1.0     ==  getfloat("\ninput 1.0, then press return: ")
+	    @test   2000.0  ==  getfloat("\ninput '2e3', then press return: ")
+	    @test   0.034   ==  getfloat("\ninput '3.4e-2', then press return: ")
+	    @test   Float64 == typeof(getfloat("\ntry to input any string, only valid number should accepted: "))
+	    @test   Float64 == typeof(getfloat("\nthe first time input '1.2f': "))
+	
+	    info("test `getfloat` with mathematical expressions...")
+	    @test   0.5           ==  getfloat("\ninput 1/2, then press return: ")
+		@test   0.75          ==  getfloat("\ninput 3//4, then press return: ")
+	    @test   Base.pi/2     ==  getfloat("\ninput 'pi/2', then press return: ")
+	    @test   Base.e        ==  getfloat("\ninput 'e', then press return: ")
+	    @test   Base.e^3      ==  getfloat("\ninput 'e^3', then press return: ")
+	    @test   Base.sin(0.1) ==  getfloat("\ninput 'sin(0.1)', then press return: ")
+	    @test   Float64       ==  typeof(getfloat("\nthe first time input '1.2+2im': "))
+
+	end #if 
+    end # testset
 	
 end # testset
