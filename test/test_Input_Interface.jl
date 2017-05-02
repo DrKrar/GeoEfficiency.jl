f2e6e6d2390b60afcc3e33717df7391c6fe462f7
Mohamed <DrKrar@users.noreply.github.com>
2017-05-03 00:25:49 +0200

    Update test_Input_Interface.jl

--- test/test_Input_Interface.jl
+++ test/test_Input_Interface.jl
@@ -16,9 +16,9 @@ const dotest = false
 @testset "Input_Interface" begin
     @testset "getfloat" begin
 	if dotest
-		info("test `getfloat` with different ways to input numbers...")
-		@test   0.0     ==  getfloat("\njust press return: ")
-		@test   1.0     ==  getfloat("\ninput 1, then press return: ")
+	    info("test `getfloat` with different ways to input numbers...")
+	    @test   0.0     ==  getfloat("\njust press return: ")
+	    @test   1.0     ==  getfloat("\ninput 1, then press return: ")
 	    @test   1.0     ==  getfloat("\ninput 1.0, then press return: ")
 	    @test   2000.0  ==  getfloat("\ninput '2e3', then press return: ")
 	    @test   0.034   ==  getfloat("\ninput '3.4e-2', then press return: ")
@@ -27,7 +27,7 @@ const dotest = false
 	
 	    info("test `getfloat` with mathematical expressions...")
 	    @test   0.5           ==  getfloat("\ninput 1/2, then press return: ")
-		@test   0.75          ==  getfloat("\ninput 3//4, then press return: ")
+	    @test   0.75          ==  getfloat("\ninput 3//4, then press return: ")
 	    @test   Base.pi/2     ==  getfloat("\ninput 'pi/2', then press return: ")
 	    @test   Base.e        ==  getfloat("\ninput 'e', then press return: ")
 	    @test   Base.e^3      ==  getfloat("\ninput 'e^3', then press return: ")
