ecdd4c4cf30fc9c84a4a3471d7afb61ac62557ae
Mohamed <DrKrar@gmail.com>
2017-05-12 20:08:13 +0200

    move .batch inside dep

--- deps/build.jl
+++ deps/build.jl
@@ -6,14 +6,15 @@ eprintln(x...) = println(STDERR, x...)
 
 # creating `GeoEfficiency` folder at the home directory.
 info("Creating `GeoEfficiency` folder at '$(homedir())'.....")
-try
-	cp(joinpath(dirname(dirname(@__FILE__)),".batch"), joinpath(homedir(),"GeoEfficiency"))
 
-catch
+try
+	cp(joinpath(dirname(@__FILE__),".batch"), joinpath(homedir(),"GeoEfficiency"))
+
+catch err
 	if isdir(joinpath(homedir(),"GeoEfficiency"))
 		warn("`GeoEfficiency` folder allready exist.")
 	else
-		warn("`GeoEfficiency` folder: could not be created.")
+		warn("`GeoEfficiency` folder: could not be created, rebuild the package `Pkg.build("GeoEfficiency")`to be able to use patch mode.)
 	end #if
 end #if
 
