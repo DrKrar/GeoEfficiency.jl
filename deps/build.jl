8cea07ea620fb6182f7aefa7f4107f3e6bb95391
Mohamed <DrKrar@users.noreply.github.com>
2017-05-16 22:56:39 +0200

    clean up build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -17,14 +17,3 @@ catch err
 		warn("""GeoEfficiency` folder: could not be created, rebuild the package `Pkg.build("GeoEfficiency")`to be able to use patch mode.""")
 	end #if
 end #if
-
-# install Package `QuadGK` for julia 0.6- or hiegher.
-#if VERSION < v"0.6.0-dev"
-#else
-#	try
-#		info("Installing Package `QuadGK` for julia 0.6- or higher .....")
-#		Pkg.add("QuadGK");
-#	catch
-#		warn("`QuadGK` package instllation did not complete")
-#	end #itryend
-#end #if
