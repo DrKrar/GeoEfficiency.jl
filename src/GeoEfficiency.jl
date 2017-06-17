e8154aaa5b4fdbcef40c79653027efe110e3967f
Mohamed <DrKrar@gmail.com>
2017-06-17 06:11:20 +0200

    code reorganization

--- src/GeoEfficiency.jl
+++ src/GeoEfficiency.jl
@@ -20,31 +20,17 @@ the source can be a point, a disc or even a cylinder.
 """
 module GeoEfficiency
 
-about() = print_with_color(:green,"""\n
-    \t *************************************************
-    \t **            -=) GeoEfficiency (=-            **
-    \t **  Accurate Geometrical Efficiency Calculator **
-    \t **             @version: v"0.8.8-dev           **
-    \t *************************************************
-
-  @author: Mohamed Krar
-  @Profile: https://www.researchgate.net/profile/Mohamed_Krar3
-  @repository: https://github.com/DrKrar/GeoEfficiency.jl/
-  @documentation: http://geoefficiencyjl.readthedocs.org
-  Created on Fri Aug 14 20:12:01 2015
-""")
-
-
-export 
+export
+ # Config
     about,
     setConsts,
 		
-    # Input_Interface
+ # Input_Interface
 	setSrcToPoint,
 	getfloat, 
 	getDetectors, 
 	
-    # Physics_Model	
+ # Physics_Model	
 	Point, 
 	source,
 	Detector, 
@@ -52,38 +38,20 @@ export
 	BoreDetector,
 	WellDetector,
 	
-    # Calculations
+ # Calculations
 	geoEff, 
 	
-    # Output_Interface	
+ # Output_Interface	
 	calc, 
 	calcN, 
 	batch
 
-function setConsts(;datafolder::Abstract string = "GeoEfficiency",
-                   resultsfolder::Abstract string = "results",
-                         relativeError::Float64 = 0.0001,
-						 absoluteError::Float64 = 0.00000000001,)
-	
-  ##Physics_Model.jl##
-
-
-  ##Intput_Interface.jl##
-  const datafolder    = datafolder
-
-  ##-Calculations.jl##
-#using QuadGK; const integrate = QuadGK.quadgk
-  const relativeError = relativeError
-  const absoluteError = absoluteError
-
-  ##Output_Interface.jl##
-  const resultsfolder = resultsfolder
-end
-
-about()
+include("Config.jl")
 include("Physics_Model.jl")
 include("Input_Interface.jl")
 include("Calculations.jl")
 include("Output_Interface.jl")
+
+about()
 
 end #module
