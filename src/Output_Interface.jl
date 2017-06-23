444c3f560bbc651b9204be0fac61e2ee210e1841
Mohamed <DrKrar@gmail.com>
2017-06-23 16:45:38 +0200

    (con.) use @__MODULE__

--- src/Output_Interface.jl
+++ src/Output_Interface.jl
@@ -6,7 +6,7 @@
 #
 #**************************************************************************************
 
-isconst(:resultsfolder) || const resultsfolder = "results";
+@compat isconst(@__MODULE__, :resultsfolder) || const resultsfolder = "results";
 const resultdir 	= joinpath(datadir, resultsfolder);		     isdir(resultdir) 		   || mkdir(resultdir)
 const resultdir_pnt = joinpath(resultdir, "Point");			     isdir(resultdir_pnt) 	 || mkdir(resultdir_pnt)
 const resultdir_nonPnt = joinpath(resultdir, "non-Point");	 isdir(resultdir_nonPnt) || mkdir(resultdir_nonPnt)
