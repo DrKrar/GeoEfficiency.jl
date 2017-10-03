7893dac21401235b0adc6bb7fbed3e07c1b56b79
Mohamed <DrKrar@users.noreply.github.com>
2017-10-03 03:16:19 +0200

    remove STDIN.buffer tests for macOS

--- test/test_Input_Interface.jl
+++ test/test_Input_Interface.jl
@@ -69,38 +69,40 @@ print("\n\t"); info("test `setSrcToPoint` & `typeofSrc`...")
 	end #testset
 
 print("\n\t"); info("test `getfloat`...")
-  @testset "getfloat" begin  
+	@testset "getfloat" begin  
 print("\n\t"); info("test `getfloat` with different ways to input numbers...")
-	@test   0.0     ==  getfloat("\njust press return: ",value="0.0")
-	@test   1.0     ==  getfloat("\ninput 1, then press return: ",value="1.0")
-	@test   1.0     ==  getfloat("\ninput 1.0, then press return: ",value="1.0")
-	@test   2000.0  ==  getfloat("\ninput '2e3', then press return: ",value="2e3")
-	@test   0.034   ==  getfloat("\ninput '3.4e-2', then press return: ",value="3.4e-2")
-	@test   isa( getfloat("\ntry to input any string, only valid number should accepted: ",value="1*0im"), Float64)
+		@test   0.0     ==  getfloat("\njust press return: ",value="0.0")
+		@test   1.0     ==  getfloat("\ninput 1, then press return: ",value="1.0")
+		@test   1.0     ==  getfloat("\ninput 1.0, then press return: ",value="1.0")
+		@test   2000.0  ==  getfloat("\ninput '2e3', then press return: ",value="2e3")
+		@test   0.034   ==  getfloat("\ninput '3.4e-2', then press return: ",value="3.4e-2")
+		@test   isa( getfloat("\ntry to input any string, only valid number should accepted: ",value="1*0im"), Float64)
 	
 
 print("\n\t"); info("test `getfloat` with mathematical expressions...")
-	@test   0.5           		==  getfloat("\ninput 1/2, then press return: ",value="1/2")
-	@test   0.75          		==  getfloat("\ninput 3//4, then press return: ",value="3//4")
-	@test   MathConstants.pi/2 	≈   getfloat("\ninput 'pi/2', then press return: ",value="pi/2")
-	@test   MathConstants.e     ≈   getfloat("\ninput 'e', then press return: ",value="e")
-	@test   MathConstants.e^3   ≈   getfloat("\ninput 'e^3', then press return: ",value="e^3")
-	@test   Base.sin(0.1) 		≈   getfloat("\ninput 'sin(0.1)', then press return: ",value="sin(0.1)")
-	for i = 0:5
-		write(STDIN.buffer,"1.2+2im\n"^i * "3\n")
-		@test   3.0 == getfloat("\nthe first time input '1.2+2im': ")
-	end # for
-	write(STDIN.buffer,"5\n" * "3\n")
-	@test   3.0 == getfloat("\ninput 1/2, then press return: ", 0.0, 4.0)
-	write(STDIN.buffer,"-1\n" * "3\n")
-	@test   3.0 == getfloat("\ninput 1/2, then press return: ", 0.0, 4.0)
-	write(STDIN.buffer,"1.2f\n" * "3\n")
-	@test   3.0 == getfloat("\nthe first time input '1.2f': ", 0.0, 4.0)
+		@test   0.5           		==  getfloat("\ninput 1/2, then press return: ",value="1/2")
+		@test   0.75          		==  getfloat("\ninput 3//4, then press return: ",value="3//4")
+		@test   MathConstants.pi/2 	≈   getfloat("\ninput 'pi/2', then press return: ",value="pi/2")
+		@test   MathConstants.e     ≈   getfloat("\ninput 'e', then press return: ",value="e")
+		@test   MathConstants.e^3   ≈   getfloat("\ninput 'e^3', then press return: ",value="e^3")
+		@test   Base.sin(0.1) 		≈   getfloat("\ninput 'sin(0.1)', then press return: ",value="sin(0.1)")
+		if !Sys.isapple()
+			for i = 0:5
+				write(STDIN.buffer,"1.2+2im\n"^i * "3\n")
+				@test   3.0 == getfloat("\nthe first time input '1.2+2im': ")
+			end # for
+			write(STDIN.buffer,"5\n" * "3\n")
+			@test   3.0 == getfloat("\ninput 1/2, then press return: ", 0.0, 4.0)
+			write(STDIN.buffer,"-1\n" * "3\n")
+			@test   3.0 == getfloat("\ninput 1/2, then press return: ", 0.0, 4.0)
+			write(STDIN.buffer,"1.2f\n" * "3\n")
+			@test   3.0 == getfloat("\nthe first time input '1.2f': ", 0.0, 4.0)
+		end #if
   end # testset
 	
 print("\n\t"); info("test `reading from CSV`...")	
     @testset "reading from CSV" begin
-	    detector_info_array = [5 0 0 0; 5 10 0 0; 5 10 2 0; 5 10 2 5]
+		detector_info_array = [5 0 0 0; 5 10 0 0; 5 10 2 0; 5 10 2 5]
         detectors = [Detector(5, 0, 0, 0), Detector(5, 10, 0, 0), Detector(5, 10, 2, 0), Detector(5, 10, 2, 5)]
 
 		datadirectory = joinpath(homedir(), "GeoEfficiency", "temptemp"); isdir(datadirectory) || mkdir(datadirectory)
@@ -201,7 +203,7 @@ print("\n\t"); info("test `getDetectors`...")
 		detector_info_array = [5 0; 10 0; 15 0; 20 0]
 		detectors = getDetectors(detector_info_array)
 		for det = detectors
-		    	@test typeof(det) == CylDetector
+			@test typeof(det) == CylDetector
 			@test typeof(det) != Detector
 		end
 		@test eltype(detectors) != CylDetector
@@ -211,7 +213,7 @@ print("\n\t"); info("test `getDetectors`...")
 		detector_info_array = [5 1; 10 1; 15 1; 20 1]
 		detectors = getDetectors(detector_info_array)
 		for det = detectors
-		    	@test typeof(det) == CylDetector
+			@test typeof(det) == CylDetector
 			@test typeof(det) != Detector
 		end
 		@test eltype(detectors) != CylDetector
@@ -222,7 +224,7 @@ print("\n\t"); info("test `getDetectors`...")
 		detector_info_array = [5 1; 10 1; 15 1; 20 1//1]
 		detectors = getDetectors(detector_info_array)
 		for det = detectors
-		    	@test typeof(det) == CylDetector
+			@test typeof(det) == CylDetector
 			@test typeof(det) != Detector
 		end
 		@test eltype(detectors) != CylDetector
@@ -233,7 +235,7 @@ print("\n\t"); info("test `getDetectors`...")
 		detector_info_array = [5 1; 10 1; 15 1; 20 1.0]
 		detectors = getDetectors(detector_info_array)
 		for det = detectors
-		    	@test typeof(det) == CylDetector
+			@test typeof(det) == CylDetector
 			@test typeof(det) != Detector
 		end
 		@test eltype(detectors) != CylDetector
