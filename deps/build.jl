2fee58f426f58e52db302876f05859cace2bb160
Mohamed <DrKrar@gmail.com>
2018-04-05 00:03:00 +0200

    more Compat.@msg

--- deps/build.jl
+++ deps/build.jl
@@ -6,15 +6,17 @@
 #
 #**************************************************************************************
 
+using Compat
+
 # creating `GeoEfficiency` folder at the home directory.
 
-info("Creating 'GeoEfficiency' folder at '$(homedir())'.....")
+Compat.@info("Creating 'GeoEfficiency' folder at '$(homedir())'.....")
 try
 	cp(joinpath(dirname(@__FILE__),".batch"), joinpath(homedir(),"GeoEfficiency"))
 catch err
 	if isdir(joinpath(homedir(),"GeoEfficiency"))
-		warn("`GeoEfficiency` folder already exist.")
+		Compat.@warn("`GeoEfficiency` folder already exist.")
 	else	
-		warn("""'GeoEfficiency' folder: could not be created,  the package may be unable to work in patch mode. Try 'Pkg.build("GeoEfficiency")' to rebuild.""")
+		Compat.@warn("""'GeoEfficiency' folder: could not be created,  the package may be unable to work in patch mode. Try 'Pkg.build("GeoEfficiency")' to rebuild.""")
 	end #if
 end #try
