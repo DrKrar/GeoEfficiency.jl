94bc6981084fdd42ae3725d36da74ae2767c9e60
Mohamed <DrKrar@gmail.com>
2018-06-06 18:05:35 +0200

    majore4 - Doc Enhancemants

--- src/GeoEfficiency.jl
+++ src/GeoEfficiency.jl
@@ -1,21 +1,20 @@
 __precompile__()
 
 """
+
 # GeoEfficiency Package
 
-GeoEfficiency Package represent a fast and flexible tool to calculate in batch or individually the geometrical efficiency
-for a set of common radiation detectors shapes (cylindrical, Bore-hole, Well-type) as seen form a source.
-the source can be a point, a disc or even a cylinder.
+`GeoEfficiency` package represent a fast and flexible tool to calculate in batch or individually 
+the geometrical efficiency for a set of common radiation detectors shapes (cylindrical, 
+Bore-hole, Well-type) as seen form a source. The source can be a point, a disc or even a cylinder.
 
 # Quick Usage
+*  geoEff()	: Calculate the geometrical efficiency for one geometrical setup return only the value of the geometrical efficiency.\n
+*  calc() 	: Calculate the geometrical efficiency for one geometrical setup and display full information on the console.\n
+*  calcN()	: Calculate the geometrical efficiency for geometrical setup(s) and display full information on the console until the user quit.\n
+*  batch()	: Calculate the geometrical efficiency using data in the **``$dataDir``** folder in batch mode.
 
- * geoEff()	: Calculate the geometrical efficiency for one geometrical setup return only the value of the geometrical efficiency.\n
-	
- * calc() 	: Calculate the geometrical efficiency for one geometrical setup and display full information on the console.\n
-	
- * calcN()	: Calculate the geometrical efficiency for geometrical setup(s) and display full information on the console until the user quit.\n
-	
- * batch()	: Calculate the geometrical efficiency using data in the "GeoEfficiency" folder in batch mode.
+**for more information and updates refer to the repository at [``GitHub.com``](https://github.com/DrKrar/GeoEfficiency.jl/)**
 
 """
 module GeoEfficiency
@@ -52,5 +51,11 @@ include("Calculations.jl")
 include("Output_Interface.jl")
 
 about()
+printstyled("""
+\nBatch mode 
+\t-  read files from directory `$dataDir`
+\t-  save results to directory `$resultdir`
+\n"""
+,  color=:green)
 
 end #module
