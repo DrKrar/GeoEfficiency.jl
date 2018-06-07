9669ff7ce42d1216e092f6bfb767a8a469e7f7d7
Mohamed <DrKrar@gmail.com>
2018-06-08 01:42:02 +0200

    update last edit

--- src/Config.jl
+++ src/Config.jl
@@ -24,7 +24,7 @@ function about()
 	\t Author:        Mohamed Krar,  @e-mail: DrKrar@gmail.com 
 	\t Auth_Profile:  https://www.researchgate.net/profile/Mohamed_Krar3
 	\t Repository:    https://github.com/DrKrar/GeoEfficiency.jl/
-	\t Version:       v"0.9.2-DEV" - ($(Date(now()) - Date("2018-06-06")) old master)  
+	\t Version:       v"0.9.2-DEV" - ($(Date(now()) - Date("2018-06-08")) old master)  
 	\t Documentation: http://geoefficiencyjl.readthedocs.org
 	\n"""
 	, color=:green)
