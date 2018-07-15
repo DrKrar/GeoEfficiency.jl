227bb64ed3010dee15fa961449897d2cd7f61e73
Mohamed <DrKrar@gmail.com>
2018-07-15 08:59:37 +0200

    fix: local "det1" twice

--- test/test_Input_Interface.jl
+++ test/test_Input_Interface.jl
@@ -300,7 +300,7 @@ end #testset_reading_from_CSV
 	end
 	@test eltype(detectors) != CylDetector
 	@test eltype(detectors) == Detector
-	local det1, det2, det3, det4 = detectors
+	det1, det2, det3, det4 = detectors
 	@test det1 <= det2 <= det3 <= det4
 		
 	detector_info_array = [5 1; 10 1; 15 1; 20 1//1]
@@ -311,7 +311,7 @@ end #testset_reading_from_CSV
 	end
 	@test eltype(detectors) != CylDetector
 	@test eltype(detectors) == Detector
-	local det1, det2, det3, det4 = detectors
+	det1, det2, det3, det4 = detectors
 	@test det1 <= det2 <= det3 <= det4
 		
 	detector_info_array = [5 1; 10 1; 15 1; 20 1.0]
@@ -322,7 +322,7 @@ end #testset_reading_from_CSV
 	end
 	@test eltype(detectors) != CylDetector
 	@test eltype(detectors) == Detector
-	local det1, det2, det3, det4 = detectors
+	det1, det2, det3, det4 = detectors
 	@test det1 <= det2 <= det3 <= det4
 
 	detector_info_array = ["5" "0" "0" "0"; "10" "0" "0" "0"]
