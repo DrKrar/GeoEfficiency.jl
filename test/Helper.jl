c4d5b49c27377d09721f28da4a6bde531cd1c85e
Mohamed <DrKrar@gmail.com>
2018-07-19 23:46:06 +0200

    let empty inout coresponding to return

--- test/Helper.jl
+++ test/Helper.jl
@@ -4,11 +4,16 @@ using Compat: stdin, @warn #, split
 function exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs::Vector; Fn_ARGs::Vector=[])
 	bffr = readavailable(stdin.buffer)
 	bffr == UInt8[] || @warn "buffer not empty, see the perivious call to `exec_consol_unattended`"  buffer = string(bffr) # empty input stream
-	
-	for input in  string.(consol_inputs)
-		 write(stdin.buffer, input, "\n")
-	end
-	return Fn(Fn_ARGs...)
+	if 0 == length(consol_inputs)
+		write(stdin.buffer, "\n")
+
+	else
+			for input in  string.(consol_inputs)
+				write(stdin.buffer, input, "\n")
+			end # for
+
+	end #IF
+	return Fn(Fn_ARGs...)		# call and return the value	
 end
 exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs...; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, consol_inputs; Fn_ARGs=Fn_ARGs)
 exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs::String; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, split(consol_inputs); Fn_ARGs=Fn_ARGs)
