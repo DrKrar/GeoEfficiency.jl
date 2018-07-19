13c8bc12476da31e01e19762a6beef69307be412
Mohamed <DrKrar@gmail.com>
2018-07-19 23:30:54 +0200

    fix typo

--- test/Helper.jl
+++ test/Helper.jl
@@ -3,7 +3,7 @@ using Compat: stdin, @warn #, split
 
 function exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs::Vector; Fn_ARGs::Vector=[])
 	bffr = readavailable(stdin.buffer)
-	bffr == UInt8[] || @warn "buffer not empty, see the perivious call to `exec_consol_unattended`"  buffer = strin(bffr) # empty input stream
+	bffr == UInt8[] || @warn "buffer not empty, see the perivious call to `exec_consol_unattended`"  buffer = string(bffr) # empty input stream
 	
 	for input in  string.(consol_inputs)
 		 write(stdin.buffer, input, "\n")
