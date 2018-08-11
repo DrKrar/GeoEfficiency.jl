e017825afc7843a55628dd3ef93d72c8cbbf6c23
Mohamed <DrKrar@users.noreply.github.com>
2018-08-11 13:12:35 +0000

    Update Output_Interface.jl

--- src/Output_Interface.jl
+++ src/Output_Interface.jl
@@ -17,7 +17,7 @@ const resultdir_pnt     = joinpath(resultdir, "Point")
 const resultdir_nonPnt  = joinpath(resultdir, "non-Point")
 const max_batch = 100   # max number of output
 const redirect  = joinpath(resultdir, "GeoEfficiency.txt"); #mkpath(redirect)
-const redirect_file = open(redirect, "w")
+const redirect_file = open(redirect, "w+")
 global countDetectors = 1
 
 
