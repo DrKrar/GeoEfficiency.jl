a5847977debf19cd82ee78df272251d08d93c230
Mohamed <DrKrar@gmail.com>
2019-02-10 04:09:47 +0200

    add comments

--- test/Helper.jl
+++ test/Helper.jl
@@ -2,12 +2,12 @@ module Helper
 using Compat
 using Compat: stdin, @warn #, split
 
-function exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs::Vector = []; Fn_ARGs::Vector=[])
-	bffr = readavailable(stdin.buffer)
-	bffr == UInt8[] || @warn "buffer not empty, see the perivious call to `exec_consol_unattended`"  buffer = String(bffr) # empty input stream
+function exec_consol_unattended(Fn::Union{Function, Type}, consol_inputs::Vector = []; Fn_ARGs::Vector=[])
+	bffr = readavailable(stdin.buffer) # empty input stream to ensure later only the `consol_inputs` is in the  tdin buffer.
+	bffr == UInt8[] || @warn "buffer not empty, see the perivious call to `exec_consol_unattended`"  buffer = String(bffr) 
 	
 	if 0 == length(consol_inputs)
-		write(stdin.buffer, "\n")
+		write(stdin.buffer, "\n")   # empty `consol_inputs` simulate return or enter .
 
 	else
 			for input in  string.(consol_inputs)
@@ -19,6 +19,7 @@ function exec_consol_unattended(Fn::Union{Function,Type}, consol_inputs::Vector 
 	return Fn(Fn_ARGs...)		# call and return the value	
 end
 exec_consol_unattended(Fn::Union{Function, Type}, consol_inputs...; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, [consol_inputs...]; Fn_ARGs=Fn_ARGs)
+exec_consol_unattended(Fn::Union{Function, Type}, consol_inputs::String; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, split(consol_inputs); Fn_ARGs=Fn_ARGs)
 
 function poly(z::Float64, coffs::Vector{Float64})
 	res::Float64 = 0.0
