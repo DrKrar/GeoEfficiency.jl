07996373ff1fcde16282c2458002790d2606e1b8
Mohamed <DrKrar@gmail.com>
2019-03-22 23:57:22 +0200

    add more tests to increase coverage

--- test/test_Error.jl
+++ test/test_Error.jl
@@ -1,14 +1,36 @@
 @debug("test Error system")
 
-@test nothing == G.@validateDetector true
-@test nothing == G.@validateDetector true "massage"
-@test nothing == G.@validateDetector true "massage1" "massage2"
+let x="1"
+    @test nothing == G.showerror(stderr, G.GeoException)
+    @test nothing == G.showerror(stderr, G.InValidDetectorDim)
+    @test nothing == G.showerror(stderr, G.NotImplementedError)
+    
 
-@test_throws  G.InValidDetectorDim  G.@validateDetector false
-@test_throws  G.InValidDetectorDim  G.@validateDetector false "massage"
-@test_throws  G.InValidDetectorDim  G.@validateDetector false "massage1" "massage2"
+    @test nothing == G.@validateDetector true
+    @test nothing == G.@validateDetector true "massage"
+    @test nothing == G.@validateDetector true "massage $x"
+    @test nothing == G.@validateDetector true "massage1" * "massage2"
+    @test nothing == G.@validateDetector true "massage1" "massage2"
+    @test nothing == G.@validateDetector true 1
+    @test nothing == G.@validateDetector true :x
+    @test nothing == G.@validateDetector true :(a+b)
+
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false "massage"
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false "massage $x"
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false "massage1" * "massage2"
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false "massage1" "massage2"
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false 1
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false :x
+    @test_throws    G.InValidDetectorDim    G.@validateDetector false :(a+b)
 
 
-@test_throws  G.NotImplementedError  G.@notImplementedError
-@test_throws  G.NotImplementedError  G.@notImplementedError "massage"
-@test_throws  G.NotImplementedError  G.@notImplementedError "massage1" "massage2"
+    @test_throws    G.NotImplementedError   G.@notImplementedError
+    @test_throws    G.NotImplementedError   G.@notImplementedError "massage"
+    @test_throws    G.NotImplementedError   G.@notImplementedError "massage $x"
+    @test_throws    G.NotImplementedError   G.@notImplementedError "massage1" "massage2"
+    @test_throws    G.NotImplementedError   G.@notImplementedError "massage1" * "massage2"
+    #@test_throws    G.NotImplementedError   G.@notImplementedError  1
+    #@test_throws    G.NotImplementedError   G.@notImplementedError  :x
+    @test_throws    G.NotImplementedError   G.@notImplementedError  :(a+b)
+end
