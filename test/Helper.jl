0f210a8d99622d1b2d372d4396f6ebf5bf55df38
Mohamed <DrKrar@gmail.com>
2019-03-28 19:02:42 +0200

    REF: remove defination of  `poly`

--- test/Helper.jl
+++ test/Helper.jl
@@ -21,17 +21,4 @@ end
 exec_consol_unattended(Fn::Union{Function, Type}, consol_inputs...; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, [consol_inputs...]; Fn_ARGs=Fn_ARGs)
 exec_consol_unattended(Fn::Union{Function, Type}, consol_inputs::String; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, split(consol_inputs); Fn_ARGs=Fn_ARGs)
 
-function poly(z::Float64, coffs::Vector{Float64})
-	res::Float64 = 0.0
-	for i= 1:length(coffs)
-		res += coffs[i] * z^(i-1)
-	end #for
-	return res
-end #function_poly
-poly0(z::Float64) = poly(z, [1.0])
-poly1(z::Float64) = poly(z, [1.0, 2.0])
-poly2(z::Float64) = poly(z, [1.0, 2.0, 3.0])
-
-end # Module
-
 const H = Helper
--- test/test_Calculations.jl
+++ test/test_Calculations.jl
@@ -9,16 +9,20 @@
 const absoluteTol1 = 1.0e-14 #1.0e-16
 const absoluteTol2 = 1.0e-11 #1.0e-13
 
+let poly0(z::Float64) = @evalpoly(z, 1.0),
+	poly1(z::Float64) = @evalpoly(z, 1.0, 2.0),
+	poly2(z::Float64) = @evalpoly(z, 1.0, 2.0, 3.0)
+	
+	@testset "integrate start=$str, end=$nd" for 
+	str = -20.0:2.0:30.0, 
+	nd  = -20.0:2.0:30.0
+	#str === nd &&  continue
 
-@testset "integrate start=$str, end=$nd" for 
-str = -20.0:2.0:30.0, 
-nd  = -20.0:2.0:30.0
-#str === nd &&  continue
-
-	@test G.integrate(H.poly0, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0) - @evalpoly(str, 0.0, 1.0) atol=absoluteTol2
-	@test G.integrate(H.poly1, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0, 1.0) - @evalpoly(str, 0.0, 1.0, 1.0) atol=absoluteTol2
-	@test G.integrate(H.poly2, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0, 1.0, 1.0) - @evalpoly(str, 0.0, 1.0, 1.0, 1.0) atol=absoluteTol2	
-end #testset_integrate
+		@test G.integrate(poly0, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0) - @evalpoly(str, 0.0, 1.0) atol=absoluteTol2
+		@test G.integrate(poly1, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0, 1.0) - @evalpoly(str, 0.0, 1.0, 1.0) atol=absoluteTol2
+		@test G.integrate(poly2, str, nd)[1] ≈ @evalpoly(nd, 0.0, 1.0, 1.0, 1.0) - @evalpoly(str, 0.0, 1.0, 1.0, 1.0) atol=absoluteTol2	
+	end #testset_integrate
+end #let
 
 
 @testset "special cases" begin
