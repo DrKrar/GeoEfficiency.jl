6f43765850aa831c0232764122698d44b1391004
Mohamed <DrKrar@gmail.com>
2019-03-30 00:18:16 +0200

    add docstrings

--- test/Helper.jl
+++ test/Helper.jl
@@ -4,22 +4,22 @@ using Compat: stdin, @warn #, split
 
 """# UnExported
 
-	@console 	expresion 	[consol_inputs...]
+	@console 	expresion 	[console_inputs...]
 
-execute the expresion `expresion` after putting `consol_inputs` into the standar input buffer.
+execute the expresion `expresion` after putting `console_inputs` into the standar input buffer.
 
 for functions that meant to run iteractivelly while require user intput, this macro provid a tool to 
-allow for noninteractive testing such functions by providing the input in advance by `consol_inputs`.
+allow for noninteractive testing such functions by providing the input in advance by `console_inputs`.
 """
-macro console(expresion, consol_inputs...)
+macro console(expresion, console_inputs...)
 	quote
-		bffr = readavailable(stdin.buffer) # empty input stream to ensure later only the `consol_inputs` is in `stdin` buffer.
+		bffr = readavailable(stdin.buffer) # empty input stream to ensure later only the `console_inputs` is in `stdin` buffer.
 		bffr == UInt8[] || @warn "buffer not empty, see the pervious process to 'stdin'"  buffer = String(bffr)
-		if 0 == length($consol_inputs)
-			write(stdin.buffer, "\n")   					# [1of2] empty `consol_inputs` simulate return or enter .
+		if 0 == length($console_inputs)
+			write(stdin.buffer, "\n")   					# [1of2] empty `console_inputs` simulate return or enter .
 		else
-			for input in $consol_inputs
-				if typeof(input) == String && input != "" 	# [2of2]empty `consol_inputs` simulate return or enter .
+			for input in $console_inputs
+				if typeof(input) == String && input != "" 	# [2of2]empty `console_inputs` simulate return or enter .
 					for npt in string.(split(input))
 						write(stdin.buffer, npt, "\n")
 					end
@@ -32,16 +32,26 @@ macro console(expresion, consol_inputs...)
 	end |> esc
 end
 
+"""# UnExported
 
-function exec_console_unattended(Fn::Union{Function, Type}, consol_inputs::Vector = []; Fn_ARGs::Vector=[])
-	bffr = readavailable(stdin.buffer) # empty input stream to ensure later only the `consol_inputs` is in `stdin` buffer.
+	exec_console_unattended(Fn::Union{Function, Type}, console_inputs::Vector = []; 
+																	Fn_ARGs::Vector=[])
+
+
+execute the expresion `Fn(Fn_ARGs...)` after putting `console_inputs` into the standar input buffer.
+
+for functions that meant to run iteractivelly while require user intput, this function provid a tool to 
+allow for noninteractive testing such functions by providing the input in advance by `console_inputs`.
+"""
+function exec_console_unattended(Fn::Union{Function, Type}, console_inputs::Vector = []; Fn_ARGs::Vector=[])
+	bffr = readavailable(stdin.buffer) # empty input stream to ensure later only the `console_inputs` is in `stdin` buffer.
 	bffr == UInt8[] || @warn "buffer not empty, see the pervious process to 'stdin'"  buffer = String(bffr) 
 	
-	if 0 == length(consol_inputs)
-		write(stdin.buffer, "\n")   # empty `consol_inputs` simulate return or enter .
+	if 0 == length(console_inputs)
+		write(stdin.buffer, "\n")   # empty `console_inputs` simulate return or enter .
 
 	else
-		for input in  string.(consol_inputs)
+		for input in  string.(console_inputs)
 			write(stdin.buffer, input, "\n") 
 		end # for
 
@@ -49,8 +59,32 @@ function exec_console_unattended(Fn::Union{Function, Type}, consol_inputs::Vecto
 	
 	return Fn(Fn_ARGs...)		# call and return the value	
 end
-exec_console_unattended(Fn::Union{Function, Type}, consol_inputs...; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, [consol_inputs...]; Fn_ARGs=Fn_ARGs)
-exec_console_unattended(Fn::Union{Function, Type}, consol_inputs::String; Fn_ARGs::Vector=[]) = exec_consol_unattended(Fn, split(consol_inputs); Fn_ARGs=Fn_ARGs)
+
+"""# UnExported
+
+	exec_console_unattended(Fn::Union{Function, Type}, console_inputs...; 
+																	Fn_ARGs::Vector=[])
+
+
+execute the expresion `Fn(Fn_ARGs...)` after putting `console_inputs` into the standar input buffer.
+
+for functions that meant to run iteractivelly while require user intput, this function provid a tool to 
+allow for noninteractive testing such functions by providing the input in advance by `console_inputs`.
+"""
+exec_console_unattended(Fn::Union{Function, Type}, console_inputs...; Fn_ARGs::Vector=[]) = exec_console_unattended(Fn, [console_inputs...]; Fn_ARGs=Fn_ARGs)
+
+"""# UnExported
+
+	exec_console_unattended(Fn::Union{Function, Type}, console_inputs::String; 
+																	Fn_ARGs::Vector=[])
+
+
+execute the expresion `Fn(Fn_ARGs...)` after putting `console_inputs` into the standar input buffer.
+
+for functions that meant to run iteractivelly while require user intput, this function provid a tool to 
+allow for noninteractive testing such functions by providing the input in advance by `console_inputs`.
+"""
+exec_console_unattended(Fn::Union{Function, Type}, console_inputs::String; Fn_ARGs::Vector=[]) = exec_console_unattended(Fn, split(console_inputs); Fn_ARGs=Fn_ARGs)
 
 end
 const H = Helper
