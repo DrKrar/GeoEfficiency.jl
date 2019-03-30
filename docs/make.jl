791f066019a80518804b7912aa6e153cedd3d613
Mohamed <DrKrar@gmail.com>
2019-03-31 01:19:55 +0200

    remove unnessary comments

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,3 @@
-#include("E:\\Users\\DrKra\\.julia\\dev\\GeoEfficiency\\docs\\make.jl")
-#=
-cdir= pwd()
-@info("Working Directory: ", @__DIR__)
-cd(@__DIR__) 
-=#
-
 #**************************************************************************************
 # make.jl
 # =============== part of the GeoEfficiency.jl package.
@@ -67,8 +60,8 @@ mktempdir() do tmp
     # Deploy HTML pages
     @info "Deploying HTML pages"
     deploydocs(
-        repo = "github.com/DrKrar/GeoEfficiency.jl",
-        versions = ["v#.#", "dev" => "dev"],
+        repo = "github.com/DrKrar/GeoEfficiency.jl.git",
+        versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
     )
     # Put back PDF into docs/build/pdf
     mkpath(joinpath(build, "pdf"))
@@ -78,71 +71,9 @@ mktempdir() do tmp
     # Deploy PDF
     @info "Deploying PDF"
     deploydocs(
-        repo = "github.com/DrKrar/GeoEfficiency.jl",
+        repo = "github.com/DrKrar/GeoEfficiency.jl.git",
         target = "build/pdf",
         branch = "gh-pages-pdf",
         forcepush = true,
     )
 end
-# cd(cdir)
-
-
-#=using Documenter, GeoEfficiency
-
-# Documenter Setup.
-
-const PAGES = [
-    "Home" => "index.md",
-    "Manual" => [
-		"manual/GeoEfficiency.md",
-        "manual/Physics_Model.md",
-        "manual/Input_Interface.md",
-        "manual/Calculations.md",
-        "manual/Output_Interface.md",
-     ],
-]
-
-makedocs(
-    modules = [GeoEfficiency],
-    clean = false,
-    format = :html,
-    sitename = "GeoEfficiency.jl",
-    authors = "Mohamed E. Krar",
-    pages = PAGES,
-)
-
-deploydocs(
-    julia = "nightly",
-    repo = "github.com/DrKrar/GeoEfficiency.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
-)=#
-
-
-#=
-# makedocs()
-makedocs(
-    build     = joinpath(pwd(), "build/html/en"),
-    modules   = [GeoEfficiency],
-    clean     = false,
-    doctest   = "doctest" in ARGS,
-    linkcheck = "linkcheck" in ARGS,
-    linkcheck_ignore = ["https://bugs.kde.org/show_bug.cgi?id=136779"], # fails to load from nanosoldier?
-    strict    = true,
-    checkdocs = :none,
-    format    = "pdf" in ARGS ? :latex : :html,
-    sitename  = "The GeoEfficiency Software Pakage",
-    authors   = "Mohamed E. Krar",
-    analytics = "UA-28835595-6",
-    pages     = PAGES,
-)
-
-deploydocs(
-	repo = "github.com/DrKrar/GeoEfficiency.git",
-	target = "build/html/en",
-	dirname = "en",
-	deps = nothing,
-	make = nothing,
-)
-=#
