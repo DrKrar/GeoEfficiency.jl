f12eb63b79d440a6075d77fd34f4167883bfabed
Mohamed <DrKrar@gmail.com>
2019-04-14 00:27:14 +0200

    remove Compat from build

--- deps/build.jl
+++ deps/build.jl
@@ -6,18 +6,17 @@
 #
 #**************************************************************************************
 
-using Compat
 
 # creating `GeoEfficiency` folder at the home directory.
 
-Compat.@info("Creating 'GeoEfficiency' folder at '$(homedir())'.....")
+@info("Creating 'GeoEfficiency' folder at '$(homedir())'.....")
 try
 	cp(joinpath(dirname(@__FILE__),".batch"), joinpath(homedir(),"GeoEfficiency"))
 catch err
 	if isdir(joinpath(homedir(),"GeoEfficiency"))
-		Compat.@warn("`GeoEfficiency` folder already exist.")
+		@warn("`GeoEfficiency` folder already exist.")
 	else	
-		Compat.@error("""'GeoEfficiency' folder: could not be created,
-		\n  the package may be unable to work in patch mode. Try 'Pkg.build("GeoEfficiency")' to rebuild.""")
+		@error("""'GeoEfficiency' folder: could not be created,
+		\n  the package may be unable to work in patch mode. Try 'import Pkg; Pkg.build("GeoEfficiency")' to rebuild.""")
 	end #if
 end #try
