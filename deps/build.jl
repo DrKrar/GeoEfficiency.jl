e5932a1ddd08dd7f90b519428582b4df30f6ed75
Mohamed Elsayed Krar <mohamed.krar@alexu.edu.eg>
2020-09-14 15:00:15 +0200

    update build process massage

--- deps/build.jl
+++ deps/build.jl
@@ -9,16 +9,17 @@
 
 # creating `.GeoEfficiency` folder at the home directory.
 
-@info("Creating 'GeoEfficiency' folder at '$(homedir())'.....")
+@info("Creating '.GeoEfficiency' data folder at '$(homedir())'.....")
 
 @info("Creating '.GeoEfficiency' folder at '$(homedir())'.....")
 try
 	cp(joinpath(dirname(@__FILE__),".batch"), joinpath(homedir(),".GeoEfficiency"))
 catch err
-	if isdir(joinpath(homedir(),"GeoEfficiency"))
-		@warn("`GeoEfficiency` folder already exist.")
+	if isdir(joinpath(homedir(),".GeoEfficiency"))
+		@warn("`.GeoEfficiency` data folder already exist in your home directory.")
 	else	
-		@error("""'GeoEfficiency' folder: could not be created,
-		\n  the package may be unable to work in patch mode. Try 'import Pkg; Pkg.build("GeoEfficiency")' to rebuild.""")
+		@error("""'.GeoEfficiency' data folder: could not be created,
+		\n  the package may be unable to work in patch mode. 
+		\n  Try 'import Pkg; Pkg.build("GeoEfficiency")' to rebuild.""")
 	end #if
 end #try
